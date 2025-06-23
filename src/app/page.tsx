a01c6506d2e0732ceada0b368e37d848fe2af81a
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-06-24 00:45:48 +0700

    feat: font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 export default function Home() {
   return (
-    <>
-      <div className="font-josefin-sans text-2xl font-thin">TES</div>
-      <div className="font-josefin-sans text-2xl font-semibold">TES</div>
-      <div className="font-josefin-sans text-2xl font-bold">TES</div>
-    </>
+    <div className="font-josefin-sans px-20 max-lg:px-18 max-md:px-12 max-sm:px-6">
+      <div className="text-h1 flex flex-col gap-4">
+        <h1 className="text-center text-h1 mb-10">Design System</h1>
+        <h1 className="text-h1">Heading 1 - 60 Semibold</h1>
+        <h2 className="text-h2">Heading 2 - 48 Semibold</h2>
+        <h3 className="text-h3">Heading 3 - 36 Semibold</h3>
+        <h4 className="text-h4">Heading 4 - 24 Semibold</h4>
+        <h5 className="text-h5">Heading 5 - 20 Semibold</h5>
+        <p className="text-headline">Headline - 20 Medium</p>
+        <p className="text-bodyLarge">Body Large - 18 Regular</p>
+        <p className="text-body">Body - 16 Regular</p>
+        <p className="text-footnote">Footnote - 14 Regular</p>
+        <p className="text-caption">Caption - 13 Regular</p>
+        <p className="text-small">Small - 12 Regular</p>
+
+        <h1 className="text-h1-mobile">Mobile Heading 1 - 60 Semibold</h1>
+        <h2 className="text-h2-mobile">Mobile Heading 2 - 48 Semibold</h2>
+        <h3 className="text-h3-mobile">Mobile Heading 3 - 36 Semibold</h3>
+        <h4 className="text-h4-mobile">Mobile Heading 4 - 24 Semibold</h4>
+        <h5 className="text-h5-mobile">Mobile Heading 5 - 20 Semibold</h5>
+        <p className="text-headline-mobile">Mobile Headline - 20 Medium</p>
+        <p className="text-bodyLarge-mobile">Mobile Body Large - 18 Regular</p>
+        <p className="text-body-mobile">Mobile Body - 16 Regular</p>
+        <p className="text-footnote-mobile">Mobile Footnote - 14 Regular</p>
+        <p className="text-caption-mobile">Mobile Caption - 13 Regular</p>
+        <p className="text-small-mobile">Mobile Small - 12 Regular</p>
+      </div>
+    </div>
   );
 }
