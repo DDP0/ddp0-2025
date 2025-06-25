953426be35990a8ad385977409077527b5517b8e
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-06-25 21:39:26 +0700

    feat: button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Disc, ArrowRight } from "lucide-react";
+
 export default function Home() {
   return (
-    <div className="font-josefin-sans px-20 max-lg:px-18 max-md:px-12 max-sm:px-6">
-      <div className="text-h1 flex flex-col gap-4">
+    <div className="bg-black text-white font-josefin-sans px-20 py-10 max-lg:px-18 max-md:px-12 max-sm:px-6">
+      <div className=" flex flex-col gap-4">
         <h1 className="text-center text-h1 mb-10">Design System</h1>
         <h1 className="text-h1">Heading 1 - 60 Semibold</h1>
         <h2 className="text-h2">Heading 2 - 48 Semibold</h2>
@@ -9,7 +12,7 @@ export default function Home() {
         <h4 className="text-h4">Heading 4 - 24 Semibold</h4>
         <h5 className="text-h5">Heading 5 - 20 Semibold</h5>
         <p className="text-headline">Headline - 20 Medium</p>
-        <p className="text-bodyLarge">Body Large - 18 Regular</p>
+        <p className="text-body-large">Body Large - 18 Regular</p>
         <p className="text-body">Body - 16 Regular</p>
         <p className="text-footnote">Footnote - 14 Regular</p>
         <p className="text-caption">Caption - 13 Regular</p>
@@ -26,6 +29,128 @@ export default function Home() {
         <p className="text-footnote-mobile">Mobile Footnote - 14 Regular</p>
         <p className="text-caption-mobile">Mobile Caption - 13 Regular</p>
         <p className="text-small-mobile">Mobile Small - 12 Regular</p>
+
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Neutral
+          <div className="w-10 h-full rounded-full bg-neutral-050"></div>
+          <div className="w-10 h-full rounded-full bg-neutral-100"></div>
+          <div className="w-10 h-full rounded-full bg-neutral-300"></div>
+          <div className="w-10 h-full rounded-full bg-neutral-500"></div>
+          <div className="w-10 h-full rounded-full bg-neutral-700"></div>
+          <div className="w-10 h-full rounded-full bg-neutral-900"></div>
+          <div className="w-10 h-full rounded-full bg-neutral-1000"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Component
+          <div className="w-10 h-full rounded-full bg-component-frame"></div>
+          <div className="w-10 h-full rounded-full bg-component-border"></div>
+          <div className="w-10 h-full rounded-full bg-component-text-white"></div>
+          <div className="w-10 h-full rounded-full bg-component-text-black"></div>
+          <div className="w-10 h-full rounded-full bg-component-error"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Card
+          <div className="w-10 h-full rounded-full bg-card"></div>
+          <div className="w-10 h-full rounded-full bg-card-hover"></div>
+          <div className="w-10 h-full rounded-full bg-card-pressed"></div>
+          <div className="w-10 h-full rounded-full bg-card-disabled"></div>
+          <div className="w-10 h-full rounded-full bg-card-red"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Intrasigent
+          <div className="w-10 h-full rounded-full bg-instrasigent-sea-050"></div>
+          <div className="w-10 h-full rounded-full bg-instrasigent-sea-100"></div>
+          <div className="w-10 h-full rounded-full bg-instrasigent-sea-300"></div>
+          <div className="w-10 h-full rounded-full bg-instrasigent-sea-500"></div>
+          <div className="w-10 h-full rounded-full bg-instrasigent-sea-700"></div>
+          <div className="w-10 h-full rounded-full bg-instrasigent-sea-900"></div>
+          <div className="w-10 h-full rounded-full bg-instrasigent-sea-1000"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Glacial
+          <div className="w-10 h-full rounded-full bg-glacial-lilac-050"></div>
+          <div className="w-10 h-full rounded-full bg-glacial-lilac-100"></div>
+          <div className="w-10 h-full rounded-full bg-glacial-lilac-300"></div>
+          <div className="w-10 h-full rounded-full bg-glacial-lilac-500"></div>
+          <div className="w-10 h-full rounded-full bg-glacial-lilac-700"></div>
+          <div className="w-10 h-full rounded-full bg-glacial-lilac-900"></div>
+          <div className="w-10 h-full rounded-full bg-glacial-lilac-1000"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Aurora
+          <div className="w-10 h-full rounded-full bg-aurora-green-050"></div>
+          <div className="w-10 h-full rounded-full bg-aurora-green-100"></div>
+          <div className="w-10 h-full rounded-full bg-aurora-green-300"></div>
+          <div className="w-10 h-full rounded-full bg-aurora-green-500"></div>
+          <div className="w-10 h-full rounded-full bg-aurora-green-700"></div>
+          <div className="w-10 h-full rounded-full bg-aurora-green-900"></div>
+          <div className="w-10 h-full rounded-full bg-aurora-green-1000"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Islan
+          <div className="w-10 h-full rounded-full bg-islan-milde-050"></div>
+          <div className="w-10 h-full rounded-full bg-islan-milde-100"></div>
+          <div className="w-10 h-full rounded-full bg-islan-milde-300"></div>
+          <div className="w-10 h-full rounded-full bg-islan-milde-500"></div>
+          <div className="w-10 h-full rounded-full bg-islan-milde-700"></div>
+          <div className="w-10 h-full rounded-full bg-islan-milde-900"></div>
+          <div className="w-10 h-full rounded-full bg-islan-milde-1000"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Retro Wave
+          <div className="w-20 h-full bg-gradient-retro-wave"></div>
+          <div className="w-20 h-full bg-gradient-retro-wave-hover"></div>
+          <div className="w-20 h-full bg-gradient-retro-wave-pressed"></div>
+          <div className="w-20 h-full bg-gradient-retro-wave-disabled"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Kiwi
+          <div className="w-20 h-full bg-gradient-kiwi"></div>
+          <div className="w-20 h-full bg-gradient-kiwi-hover"></div>
+          <div className="w-20 h-full bg-gradient-kiwi-pressed"></div>
+          <div className="w-20 h-full bg-gradient-kiwi-disabled"></div>
+        </div>
+        <div className="h-30 w-full p-10 rounded-3xl text-h3 items-center bg-white/50 flex justify-between">
+          Linear White
+          <div className="w-20 h-full bg-gradient-linear-white"></div>
+          <div className="w-20 h-full bg-gradient-linear-white-hover"></div>
+          <div className="w-20 h-full bg-gradient-linear-white-pressed"></div>
+          <div className="w-20 h-full bg-gradient-linear-white-disabled"></div>
+        </div>
+        {/* <Button className="">Primary</Button> */}
+        {/* <div className="absolute top-20 w-full h-100 rounded-2xl bg-border p-[1px]"> */}
+        <div className="absolute glass top-20 w-full h-100 rounded-2xl border border-card-disabled p-[1px]"></div>
+        <Button className="w-full">
+          <Disc />
+          Button
+          <ArrowRight />
+        </Button>
+        <Button className="w-full" variant={"blue"}>
+          <Disc />
+          Button
+          <ArrowRight />
+        </Button>
+        <Button className="w-full" variant={"lilac"}>
+          <Disc />
+          Button
+          <ArrowRight />
+        </Button>
+        <Button className="w-full" variant={"yellow"}>
+          <Disc />
+          Button
+          <ArrowRight />
+        </Button>
+        <Button className="w-full" variant={"retro"}>
+          <Disc />
+          Button
+          <ArrowRight />
+        </Button>
+        <Button className="w-full" variant={"kiwi"}>
+          <Disc />
+          Button
+          <ArrowRight />
+        </Button>
+        <div className="bg-gradient-retro-wave"></div>
       </div>
     </div>
   );
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex font-josefine-sans border-2 border-[#ffffff59] items-center justify-center gap-2 whitespace-nowrap rounded-xl transition-all disabled:cursor-not-allowed disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive text-neutral-050 active:text-neutral-300 disabled:text-neutral-500",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-card/10 hover:bg-card-hover/30 active:bg-card-pressed/50 disabled:bg-card-disabled/50",
+        blue: "bg-[#81beff4d] hover:bg-[#b3dfff4d] active:bg-[#ccecff4d] disabled:bg-[#e5f6ff4d]",
+        lilac:
+          "bg-[#c99bdb4d] hover:bg-[#f6d5ff4d] active:bg-[#fde5ff4d] disabled:bg-[#ffeefe4d]",
+        yellow:
+          "bg-[#fec8884d] hover:bg-[#fee4b74d] active:bg-[#feefcf4d] disabled:bg-[#fef8e74d]",
+        retro:
+          "bg-gradient-retro-button hover:bg-gradient-retro-button-hover active:bg-gradient-retro-button-pressed disabled:bg-gradient-retro-button-disabled",
+        kiwi:
+          "bg-gradient-kiwi-button hover:bg-gradient-kiwi-button-hover active:bg-gradient-kiwi-button-pressed disabled:bg-gradient-retro-kiwi-disabled",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "py-2 px-4 has-[>svg]:px-3",
+        sm: "py-1 px-2 has-[>svg]:px-3",
+        lg: "py-3 px-6 has-[>svg]:px-3",
+        icon: "size-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <div>
+      <Comp
+        data-slot="button"
+        className={cn(
+          "cursor-pointer glass text-[18px] max-sm:text-[16px]",
+          buttonVariants({ variant, size, className })
+        )}
+        {...props}
+      />
+    </div>
+  );
+}
+
+export { Button, buttonVariants };
