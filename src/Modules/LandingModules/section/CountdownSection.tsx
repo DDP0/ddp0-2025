bc8819492d66b0460c36572ca38aed3dd19bfeb2
Hakim Nizami <hakimnizami15@gmail.com>
2025-06-27 21:28:43 +0700

    feat: added assets

--- src/Modules/LandingModules/section/CountdownSection.tsx
+++ src/Modules/LandingModules/section/CountdownSection.tsx
@@ -4,8 +4,15 @@ import { ChevronRight, Search } from "lucide-react";
 
 const CountdownSection = () => {
   return (
-    <section className="relative min-h-screen flex flex-col gap-8 justify-center items-center font-josefin-sans">
-      <img src="/swirl.webp" alt="swirl" className="absolute" />
+    <section className="relative aspect-video flex flex-col gap-8 justify-center items-center font-josefin-sans overflow-x-hidden">
+      <div className="w-1/6 aspect-square rounded-full blur-3xl absolute -left-24 top-1/2 -translate-y-2/3 bg-gradient-retro-button" />
+      <div className="w-1/6 aspect-square rounded-full blur-3xl absolute right-0 top-0 translate-x-1/2 -translate-y-1/2 bg-[#18357A]" />
+      <img src="/swirl.webp" alt="swirl" className="absolute bottom-0" />
+      <img
+        src="/buwlan.webp"
+        alt="buwlan"
+        className="absolute w-1/4 left-0 -translate-x-1/2 translate-y-1/3"
+      />
       <h1 className="text-h1">Countdown</h1>
       <div className="flex flex-col gap-4 items-center border border-border/20 rounded-3xl bg-gradient-to-r from-islan-milde-500/20 to-aurora-green-500/20 backdrop-blur-md px-20 py-8">
         <h1 className="text-h5 opacity-80">Kelompok Mentoring</h1>
