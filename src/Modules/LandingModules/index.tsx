6c5ae9dea6931e90d996bb1b542b21ed4475a1ca
anotherbondan <anandagautama3112@gmail.com>
2025-06-27 11:55:48 +0700

    feat: description text

--- src/Modules/LandingModules/index.tsx
+++ src/Modules/LandingModules/index.tsx
@@ -1,4 +1,11 @@
+import WhatIsDDP0 from "./section/WhatIsDDP0";
+
 const LandingModules = () => {
-  return <main>This is landing</main>;
+  return (
+    <main>
+      This is landing
+      <WhatIsDDP0 />
+    </main>
+  );
 };
 export default LandingModules;
---
+++ src/Modules/LandingModules/section/WhatIsDDP0.tsx
@@ -0,0 +1,31 @@
+export default function WhatIsDDP0() {
+  return (
+    <section className="flex flex-col justify-center mt-16">
+      <div className="flex flex-col justify-center gap-12 w-full px-4">
+        <h1 className="font-josefin-sans text-h1-mobile text-center">
+          What Is <span className="bg-gradient-retro-wave bg-clip-text text-transparent">DDP-0?</span>
+        </h1>
+
+        <div className="px-4">
+          <p className="text-headline-mobile font-josefin-sans text-center">
+            DDP-0 adalah program pembekalan dari COSMIC 2024 untuk
+            memperkenalkan konsep dasar pemrograman kepada mahasiswa baru CSUI
+            2025, sebagai bekal menghadapi perkuliahan di Fasilkom UI
+          </p>
+          <br />
+          <p className="text-headline-mobile font-josefin-sans text-center">
+            Melalui program ini, mahasiswa baru CSUI 2025 diharapkan dapat
+            membangun fondasi yang kuat dan lebih percaya diri menghadapi dunia
+            pemrograman di bangku kuliah.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+}
