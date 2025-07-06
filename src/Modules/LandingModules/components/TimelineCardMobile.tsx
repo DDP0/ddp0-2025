2949f9d841a6b6dad5505292c20bb3fb3ae3f92f
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-07-07 06:38:32 +0700

    chore: add key

--- src/Modules/LandingModules/components/TimelineCardMobile.tsx
+++ src/Modules/LandingModules/components/TimelineCardMobile.tsx
@@ -1,11 +1,9 @@
-
-
-export default function TimelineCardMobile({lines}:{lines: string[]}) {
+export default function TimelineCardMobile({ lines }: { lines: string[] }) {
   return (
     <div className="text-center w-fit border px-4 py-2 text-bodyLarge-mobile font-josefin-sans rounded-lg glass shadow-xl">
-      {
-        lines.map((e) => <h4>{e}</h4>)
-      }
+      {lines.map((e, index) => (
+        <h4 key={index}>{e}</h4>
+      ))}
     </div>
-  )
+  );
 }
--- src/Modules/LandingModules/index.tsx
+++ src/Modules/LandingModules/index.tsx
@@ -3,12 +3,14 @@ import FAQPage from "./section/Faq";
 import TestimonyPage from "./section/Testimony";
 import WhatIsDDP0 from "./section/WhatIsDDP0";
 import Hero from "./section/Hero";
+import Timeline from "./section/Timeline";
 
 const LandingModules = () => {
   return (
     <main className="flex flex-col gap-40 overflow-hidden">
       <Hero />
       <WhatIsDDP0 />
+      <Timeline />
       <CountdownSection />
       <TestimonyPage />
       <FAQPage />
