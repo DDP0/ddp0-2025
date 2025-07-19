dfa262df71c8647688df18fd0884662dcb4efa1d
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-07-19 19:44:16 +0900

    fix: add backdrop-blur to card

--- src/Modules/LandingModules/components/TimelineCard.tsx
+++ src/Modules/LandingModules/components/TimelineCard.tsx
@@ -2,7 +2,7 @@
 
 export default function TimelineCard({lines}:{lines: string[]}) {
   return (
-    <div className="text-center w-fit border px-4 py-2 text-bodyLarge font-josefin-sans rounded-lg glass shadow-xl max-xl:text-body-mobile max-lg:text-[12px] max-lg:px-2 max-lg:py-1">
+    <div className="text-center w-fit border px-4 py-2 text-bodyLarge font-josefin-sans rounded-lg glass backdrop-blur-lg shadow-xl max-xl:text-body-mobile max-lg:text-[12px] max-lg:px-2 max-lg:py-1">
       {
         lines.map((e) => <h4>{e}</h4>)
       }
