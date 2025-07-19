48735b6ab741dab10c12d0033eb7dcbe03745db6
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-07-19 11:59:07 +0700

    few adjustment

--- src/Modules/LandingModules/section/Testimony.tsx
+++ src/Modules/LandingModules/section/Testimony.tsx
@@ -3,31 +3,79 @@ import Image from "next/image";
 const testimonies = [
   {
     img: "/testimony/faiq.jpg",
-    name: "Ahmad Faiq Fawwaz",
+    name: "Ahmad Faiq",
     desc: "Ilmu Komputer '24",
     testimony:
       "DDP-0 IS FUN!! apalagi ddp-0 ini sangat membantu bagi gw yang 0 besar dalam dunia perkodingan saat pertama kali masuk pacil dan gw juga bisa dapet insight dari mentor gw yang asik 😆",
   },
   {
-    img: "/testimony/faiq.jpg",
-    name: "Ahmad Faiq Fawwaz",
-    desc: "Ilmu Komputer '24",
+    img: "/testimony/farrel.jpg",
+    name: "Farrel Arrayyan",
+    desc: "Sistem Informasi '24",
     testimony:
-      "DDP-0 IS FUN!! apalagi ddp-0 ini sangat membantu bagi gw yang 0 besar dalam dunia perkodingan saat pertama kali masuk pacil dan gw juga bisa dapet insight dari mentor gw yang asik 😆",
+      "Sebagai orang yang belum punya basic programming sebelumnya, DDP-0 membantu banget untuk persiapan kuliah! Materinya dijelasin dari nol oleh mentor-mentor yang asik dan gampang untuk dipahami. Tugas-tugasnya juga seru banget jadi gak terasa membebani. Selain belajar programming, DDP-0 juga bisa jadi tempat kenalan sama temen-temen seangkatan maupun kating kalian lho guys!",
   },
   {
-    img: "/testimony/faiq.jpg",
-    name: "Ahmad Faiq Fawwaz",
+    img: "/testimony/azka.png",
+    name: "Muhammad Azka",
     desc: "Ilmu Komputer '24",
     testimony:
-      "DDP-0 IS FUN!! apalagi ddp-0 ini sangat membantu bagi gw yang 0 besar dalam dunia perkodingan saat pertama kali masuk pacil dan gw juga bisa dapet insight dari mentor gw yang asik 😆",
+      "Jujur klo semisal gw gaikut ddp0, ga kebayang seberapa keteterannya nanti gw pas ddp1. Buat maba2 fasilkom, ikut ddp0 ✨is a must ✨karena kalian bakal ngerasain gambaran medan tempur ddp, jadi di ddp0 tempat buat warm up tipis2 ajah. DDP itu bukan tentang cara bisa ngoding pake suatu bahasa, tp bagaimana kalian problem solving pake bahasa yg dipelajari. Tapi jangan takut, mentor2 siap bikin kalian lebih siap menghadapi DDP1.",
   },
   {
-    img: "/testimony/faiq.jpg",
-    name: "Ahmad Faiq Fawwaz",
+    img: "/testimony/anya.jpg",
+    name: "Anya Aleena",
     desc: "Ilmu Komputer '24",
     testimony:
-      "DDP-0 IS FUN!! apalagi ddp-0 ini sangat membantu bagi gw yang 0 besar dalam dunia perkodingan saat pertama kali masuk pacil dan gw juga bisa dapet insight dari mentor gw yang asik 😆",
+      "Buat orang yang belum pernah ngoding sebelumnya, DDP-0 ngebantu banget buat dapetin feel pas ngoding, jadi 'click' gitu. Terus juga relasinya oke banget, kating yang dulu jadi mentorku masih sering bantuin waktu DDP-1 dan bahkan jadi dekat sampai sekarang!!",
+  },
+  {
+    img: "/testimony/rama.png",
+    name: "Walyul'ahdi Maulana",
+    desc: "Sistem Informasi '24",
+    testimony:
+      "Pengalaman yang sangat seru dan bermanfaat buat dapetin spoiler matkul DDP-1, alhasil ga begitu kaget dan kesusahan ke depannya. Btw aku dulu dapet mentor yg keren dan baik banget, seneng rasanya dapet apresiasi bahkan buat hal sekecil ini.",
+  },
+  {
+    img: "/testimony/hanif.jpeg",
+    name: "Hanif Awiyoso",
+    desc: "Ilmu Komputer '24",
+    testimony:
+      "DDP0 ini ngebantu bangett sihh, karena materi yang diajarin mentor2 mostly cover materi DDP1! Jadi bagi kalian yang ikut DDP0, dijamin ga bakal kewalahan dgn DDP1!!",
+  },
+  {
+    img: "/testimony/nathanael.jpg",
+    name: "Nathanael Leander",
+    desc: "Ilmu Komputer '24",
+    testimony: "Semoga bisa memberikan yang terbaik",
+  },
+  {
+    img: "/testimony/nadhif.jpeg",
+    name: "Muhammad Nadhif",
+    desc: "Ilmu Komputer '24",
+    testimony:
+      "Terima kasih banyak DDP0 sudah membantu saya dalam belajar programming dari 0, karena saya baru punya laptop dan belum belajar sama sekali saat itu. Seandainya saya tidak ikut DDP0, mungkin saya tidak akan lulus DDP1 dan DDP2 ❤️",
+  },
+  {
+    img: "/testimony/raida.png",
+    name: "Raida Khoyyara",
+    desc: "Ilmu Komputer '24",
+    testimony:
+      "Buat orang yang sama sekali gapunya basic coding even gatau kenapa milih jurusan ini, DDP0 HELPS ME A LOT. Di DDP0 belajarnya juga lebih enjoy karena yang mentoring kating dan lebih enak nanya nanya nya, dapet temen baru, wawasan baru tentang fasilkom jugaa.",
+  },
+  {
+    img: "/testimony/andrew.png",
+    name: "Andrew Panjaitan",
+    desc: "Sistem Informasi '24",
+    testimony:
+      "Seru banget, Selain DDP-0 menjadi pendorong pemahaman ku di python (izin pamer ddp-1 gw jadi A 😎), bisa bonding dengan kating dan teman-teman seangkatann, shoutout to kelompok 045 kecee 🔥",
+  },
+  {
+    img: "/testimony/jessica.jpg",
+    name: "Jessica Tandra",
+    desc: "Ilmu Komputer '24",
+    testimony:
+      "Kalo ditanya nyesel ga ikut ddp-0?, jujur... NGGA SAMA SEKALIII. Sesuai namanya, DDP-0 ya tentunya kalian akan diajarin ngoding bnr2 dari 0. Selain itu, semua sistem tugas, quiz maupun lab-nya mirip banget sama DDP 1. So DDP-0 ini akan sangattt membantu kalian nantinyaa. Oh iya kalian juga bisa kenalan sama temen baru + kating jugaa. Tunggu apa lagi? yukk buruan joinnn! (jangan sampe fomo yaa :P)",
   },
 ];
 
--- src/components/elements/TestimonialsCard/index.tsx
+++ src/components/elements/TestimonialsCard/index.tsx
@@ -14,13 +14,28 @@ const TestimonialCard = ({
 }) => {
   return (
     <div className="relative max-w-md w-full scale-[0.95] sm:scale-100 transition-transform duration-300 ease-in-out">
+      {/* Main Card */}
+      <div className="relative z-0 flex flex-col justify-start items-center bg-black/40 backdrop-blur-xl border border-white/20 rounded-2xl p-6 sm:p-8 pb-25 shadow-2xl ring-1 ring-white/10 min-h-[22rem] overflow-hidden before:content-[''] before:absolute before:inset-0 before:rounded-2xl before:pointer-events-none before:bg-[radial-gradient(circle,rgba(255,255,255,0.1)_0%,transparent_80%)] before:opacity-40 before:blur-md">
+        {/* Quote Symbol */}
+        <div className="text-5xl sm:text-6xl font-serif mb-4 sm:mb-6">
+          <Image src="/testimonial_quotes.svg" width={60} height={60} alt="" />
+        </div>
+
+        {/* Testimonial Text */}
+        <div className="text-white/90 h-50 no-scrollbar text-sm sm:text-base leading-relaxed mb-8 text-center font-josefin-sans overflow-y-scroll">
+          <p>{testimony}</p>
+        </div>
+      </div>
+
       {/* Profile Section */}
       <div className="absolute z-10 -bottom-8 -left-6 sm:-left-8 flex items-center">
-        <div className="w-fit h-full p-0.5 bg-gradient-linear-white rounded-full">
-          <div className="relative w-20 h-20 sm:w-26 sm:h-26 rounded-full overflow-hidden bg-gradient-linear-white shadow-lg">
-            {/* <div className="relative w-full h-full rounded-full bg-gray-300 overflow-hidden"> */}
-            <img src={img} alt="Profile" className="object-cover w-full h-full" />
-            {/* </div> */}
+        <div className="w-20 h-20 sm:w-26 sm:h-26 rounded-full bg-gradient-linear-white from-purple-400 to-pink-400 p-0.5 shadow-lg">
+          <div className="w-full h-full rounded-full bg-gray-300 overflow-hidden">
+            <img
+              src={img}
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
           </div>
         </div>
 
@@ -31,18 +46,6 @@ const TestimonialCard = ({
           <p className="text-white/70 text-xs sm:text-sm font-josefin-sans">
             {desc}
           </p>
-        </div>
-      </div>
-      {/* Main Card */}
-      <div className="relative z-0 flex flex-col justify-start items-center bg-black/40 backdrop-blur-xl border border-white/20 rounded-2xl p-6 sm:p-8 pb-14 shadow-2xl ring-1 ring-white/10 min-h-[22rem] overflow-hidden before:content-[''] before:absolute before:inset-0 before:rounded-2xl before:pointer-events-none before:bg-[radial-gradient(circle,rgba(255,255,255,0.1)_0%,transparent_80%)] before:opacity-40 before:blur-md">
-        {/* Quote Symbol */}
-        <div className="text-5xl sm:text-6xl font-serif mb-4 sm:mb-6">
-          <Image src="/testimonial_quotes.svg" width={60} height={60} alt="" />
-        </div>
-
-        {/* Testimonial Text */}
-        <div className="text-white/90 text-sm sm:text-base leading-relaxed mb-8 text-center font-josefin-sans">
-          <p>{testimony}</p>
         </div>
       </div>
     </div>
