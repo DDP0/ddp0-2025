ccb333bf44018679b1778bb6e483f7b20dbc9d10
Kevin Cornellius Widjaja <kevinbananax@gmail.com>
2025-07-19 12:30:54 +0700

    fix: safari backdrop-blur behaviour

--- src/components/elements/TestimonialsCard/index.tsx
+++ src/components/elements/TestimonialsCard/index.tsx
@@ -15,20 +15,20 @@ const TestimonialCard = ({
   return (
     <div className="relative max-w-md w-full scale-[0.95] sm:scale-100 transition-transform duration-300 ease-in-out">
       {/* Main Card */}
-      <div className="relative z-0 flex flex-col justify-start items-center bg-black/40 backdrop-blur-xl border border-white/20 rounded-2xl p-6 sm:p-8 pb-14 shadow-2xl ring-1 ring-white/10 min-h-[22rem] overflow-hidden before:content-[''] before:absolute before:inset-0 before:rounded-2xl before:pointer-events-none before:bg-[radial-gradient(circle,rgba(255,255,255,0.1)_0%,transparent_80%)] before:opacity-40 before:blur-md">
+      <div className="relative z-40 flex flex-col justify-start items-center opacity-[99%] bg-black/40  border border-white/20 rounded-2xl p-6 sm:p-8 pb-14 shadow-2xl ring-1 ring-white/10 min-h-[22rem] overflow-hidden before:content-[''] before:absolute before:inset-0 before:rounded-2xl before:pointer-events-none before:bg-[radial-gradient(circle,rgba(255,255,255,0.1)_0%,transparent_80%)] before:opacity-40 before:blur-md before:z-10">
         {/* Quote Symbol */}
         <div className="text-5xl sm:text-6xl font-serif mb-4 sm:mb-6">
           <Image src="/testimonial_quotes.svg" width={60} height={60} alt="" />
         </div>
 
         {/* Testimonial Text */}
-        <div className="text-white/90 h-50 no-scrollbar text-sm sm:text-base leading-relaxed mb-8 text-center font-josefin-sans overflow-y-scroll">
+        <div className="text-white/90 text-sm sm:text-base leading-relaxed mb-8 text-center font-josefin-sans">
           <p>{testimony}</p>
         </div>
       </div>
 
       {/* Profile Section */}
-      <div className="absolute z-10 -bottom-8 -left-6 sm:-left-8 flex items-center">
+      <div className="absolute -bottom-8 -left-6 sm:-left-8 flex items-center z-50">
         <div className="w-20 h-20 sm:w-26 sm:h-26 rounded-full bg-gradient-linear-white from-purple-400 to-pink-400 p-0.5 shadow-lg">
           <div className="w-full h-full rounded-full bg-gray-300 overflow-hidden">
             <img
