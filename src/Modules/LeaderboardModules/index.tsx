4dfd89ec78b3a88114a60f61102b5496ab1b064f
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-07-31 02:02:16 +0700

    feat: leaderboard

--- src/Modules/LeaderboardModules/index.tsx
+++ src/Modules/LeaderboardModules/index.tsx
@@ -18,7 +18,7 @@ const LeaderboardModule = ({
     );
   }
   return (
-    <main className="min-h-screen w-full relative flex flex-col py-32 max-md:py-28 px-20 max-lg:px-14 max-md:px-10 max-sm:px-5 font-josefin-sans">
+    <main className="min-h-screen w-full relative flex flex-col gap-6 py-32 max-md:py-28 px-20 max-lg:px-14 max-md:px-10 max-sm:px-5 font-josefin-sans overflow-hidden">
       {/* top 3 */}
       <div className="flex flex-col gap-6">
         <h1 className="text-h3 max-md:text-h3-mobile text-center">
@@ -26,14 +26,14 @@ const LeaderboardModule = ({
         </h1>
         <div className="w-full h-110 max-lg:h-100 grid grid-cols-3 gap-10 max-lg:gap-8 max-md:gap-4 max-sm:gap-2 relative">
           {/* second place */}
-          <div className="w-full h-full flex flex-col-reverse gap-3">
+          <div className="w-full h-full flex flex-col-reverse gap-3 hover:opacity-80 transition-opacity duration-300 group">
             <div className="h-[50%] w-full bg-gradient-podium  rounded-t-3xl border-t border-x border-[#ffffff59] flex flex-col items-center py-6 max-md:py-3 justify-between">
               <div
                 style={{
                   background:
                     "linear-gradient(35deg, rgba(255, 255, 255, 0.00) 33.61%, #FFF 89.19%), rgba(255, 255, 255, 0.08)",
                 }}
-                className="rounded-full w-12 h-12 max-md:w-10 max-md:h-10 overflow-hidden p-[1px]"
+                className="rounded-full w-12 h-12 max-md:w-10 max-md:h-10 overflow-hidden p-[1px] group-hover:-translate-y-2 transition-all duration-300"
               >
                 <div
                   style={{
@@ -54,7 +54,12 @@ const LeaderboardModule = ({
             </div>
             <div className="text-center flex flex-col gap-2">
               <p className="text-h5-mobile max-md:text-bodyLarge-mobile">
-                {leaderboardData[1].user.name}
+                {leaderboardData[1].user.name.length > 15
+                  ? leaderboardData[1].user.name
+                      .split(" ")
+                      .slice(0, 2)
+                      .join(" ")
+                  : leaderboardData[1].user.name}
               </p>
               <p className="text-bodyLarge-mobile max-md:text-footnote-mobile">
                 {leaderboardData[1].user.kelompok}
@@ -71,14 +76,14 @@ const LeaderboardModule = ({
             </div>
           </div>
           {/* first place */}
-          <div className="w-full h-full flex flex-col-reverse gap-3">
+          <div className="w-full h-full flex flex-col-reverse gap-3 hover:opacity-80 transition-opacity duration-300 group">
             <div className="h-[60%] w-full bg-gradient-podium  rounded-t-3xl border-t border-x border-[#ffffff59] flex flex-col items-center py-6 max-md:py-3 justify-between">
               <div
                 style={{
                   background:
                     "linear-gradient(35deg, rgba(255, 255, 255, 0.00) 33.61%, #FFF 89.19%), rgba(255, 255, 255, 0.08)",
                 }}
-                className="rounded-full w-12 h-12 max-md:w-10 max-md:h-10 overflow-hidden p-[1px]"
+                className="rounded-full w-12 h-12 max-md:w-10 max-md:h-10 overflow-hidden p-[1px] group-hover:-translate-y-2 transition-all duration-300"
               >
                 <div
                   style={{
@@ -100,7 +105,12 @@ const LeaderboardModule = ({
             </div>
             <div className="text-center flex flex-col gap-2">
               <p className="text-h5-mobile max-md:text-bodyLarge-mobile">
-                {leaderboardData[0].user.name}
+                {leaderboardData[0].user.name.length > 15
+                  ? leaderboardData[0].user.name
+                      .split(" ")
+                      .slice(0, 2)
+                      .join(" ")
+                  : leaderboardData[0].user.name}
               </p>
               <p className="text-bodyLarge-mobile max-md:text-footnote-mobile">
                 {leaderboardData[0].user.kelompok}
@@ -117,14 +127,14 @@ const LeaderboardModule = ({
             </div>
           </div>
           {/* third place */}
-          <div className="w-full h-full flex flex-col-reverse gap-3">
+          <div className="w-full h-full flex flex-col-reverse gap-3 hover:opacity-80 transition-opacity duration-300 group">
             <div className="h-[40%] w-full bg-gradient-podium  rounded-t-3xl border-t border-x border-[#ffffff59] flex flex-col items-center py-6 max-md:py-3 justify-between">
               <div
                 style={{
                   background:
                     "linear-gradient(35deg, rgba(255, 255, 255, 0.00) 33.61%, #FFF 89.19%), rgba(255, 255, 255, 0.08)",
                 }}
-                className="rounded-full w-12 h-12 max-md:w-10 max-md:h-10 overflow-hidden p-[1px]"
+                className="rounded-full w-12 h-12 max-md:w-10 max-md:h-10 overflow-hidden p-[1px] group-hover:-translate-y-2 transition-all duration-300"
               >
                 <div
                   style={{
@@ -145,7 +155,12 @@ const LeaderboardModule = ({
             </div>
             <div className="text-center flex flex-col gap-2">
               <p className="text-h5-mobile max-md:text-bodyLarge-mobile">
-                {leaderboardData[2].user.name}
+                {leaderboardData[2].user.name.length > 15
+                  ? leaderboardData[2].user.name
+                      .split(" ")
+                      .slice(0, 2)
+                      .join(" ")
+                  : leaderboardData[2].user.name}
               </p>
               <p className="text-bodyLarge-mobile max-md:text-footnote-mobile">
                 {leaderboardData[2].user.kelompok}
@@ -164,6 +179,45 @@ const LeaderboardModule = ({
           {/* blur */}
           <div className="absolute bottom-0 -translate-x-[5%] self-center w-[110%] h-15 max-sm:h-10 translate-y-1/2 rounded-full bg-gradient-retro-wave-pressed opacity-70 blur-[35px]"></div>
         </div>
+      </div>
+      {/* end top 3 */}
+
+      {/* rest of leaderboard */}
+      <div className="w-full flex flex-col gap-3 max-md:gap-2 p-6 max-md:p-3 rounded-3xl glass border border-[#ffffff59]">
+        {leaderboardData.slice(3).map((item, index) => (
+          <div
+            key={index}
+            className="w-full flex gap-3 max-lg:gap-2 max-md:gap-1"
+          >
+            <div className="rounded-xl glass bg-card/10 flex justify-center items-center w-12 h-12 max-md:w-12 max-md:h-8 border border-[#ffffff59]">
+              <span className="leading-tight text-h4 max-md:text-headline-mobile">
+                {index + 4}
+              </span>
+            </div>
+            <div className="rounded-xl flex px-3 items-center w-full glass bg-card/10 h-12 max-md:h-8 border border-[#ffffff59]">
+              <span className="leading-tight text-bodyLarge max-md:text-body-mobile">
+                {item.user.name.length > 15
+                  ? item.user.name.split(" ").slice(0, 2).join(" ")
+                  : item.user.name}
+              </span>
+            </div>
+            <div className="rounded-xl glass bg-card/10 flex justify-center items-center w-40 px-3 h-12 max-md:w-20 max-md:h-8 border border-[#ffffff59]">
+              <span className="leading-tight max-md:hidden text-bodyLarge max-md:text-body-mobile">
+                {item.user.kelompok}
+              </span>
+              <span className="leading-tight hidden max-md:flex text-bodyLarge max-md:text-body-mobile">
+                {item.user.kelompok.replace("Kelompok ", "")}
+              </span>
+            </div>
+            <div className="bg-gradient-kiwi p-[1px] w-20 max-md:w-18 h-12 max-md:h-8 rounded-xl">
+              <div className="bg-card w-full h-full flex justify-center items-center glass rounded-xl px-3 py-1">
+                <span className="font-josefin-sans text-transparent bg-gradient-kiwi bg-clip-text text-h5 max-md:text-h5-mobile leading-tight">
+                  {item.totalScore}
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
       <Background />
     </main>
