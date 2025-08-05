3ac77709be6aa43d41ba98dcf9785cd8cd72489f
Vin-Ren <reaxumrx@gmail.com>
2025-08-05 18:34:04 +0900

    fix: dialog title required

--- src/Modules/MerchModules/MerchDetailsModule/components/SizeGuideDialog.tsx
+++ src/Modules/MerchModules/MerchDetailsModule/components/SizeGuideDialog.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import CocoLineArrow from "./CocoLineArrowRight";
 import { Button } from "@/components/ui/button";
 import CocoLineClose from "./CocoLineClose";
@@ -11,6 +11,8 @@ export default function SizeGuideDialog() {
         <Button variant={'blue'} className="gap-3">Size Guide <CocoLineArrow /></Button>
       </DialogTrigger>
       <DialogContent className="bg-black glass fill text-accent border-[0.5px]" showCloseButton={false}>
+        <DialogTitle>
+        </DialogTitle>
         <div className=" flex flex-col justify-between gap-4 text-end">
           <DialogClose asChild>
             <Button className="w-10 h-10 p-0">
